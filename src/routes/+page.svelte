c989c87fd0e560d949b13bb77b11da13a0518577
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-05-06 11:18:04 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let count = $state(0);
+</script>
+
+<h1>Counter</h1>
+<button onclick={() => count += 1}>Increment</button>
+<p>Count: {count}</p>
+<p>Click the button to increment the count.</p>
